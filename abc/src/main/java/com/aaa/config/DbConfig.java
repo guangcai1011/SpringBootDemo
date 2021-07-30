ea418ea753da0b07ffe4e13c2403f8e17957e8ff
wgc1011 <1763452998@qq.com>
2021-07-30 14:12:42 +0800

    change project

--- abc/src/main/java/com/aaa/config/DbConfig.java
+++
@@ -1,5 +0,0 @@
-package com.aaa.config;
-
-public class DbConfig {
-    
-}
--- src/test/java/com/wgc/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/wgc/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.aaa.abc;
+package com.wgc.springbootdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AbcApplicationTests {
+class SpringBootDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
